f4911a5e92f7b642dfb14b42cab2751e4081b373
jannehakala <janne.hakala94@hotmail.fi>
2016-03-21 08:53:06 +0200

    Add ui files

---
+++ PHP-IIM50300/footer.php
@@ -0,0 +1,6 @@
+﻿	<footer>
+		      <div id="footerBar"><span id="copyright">&copy; Music database 2016</span>
+		      <button class="buttons" id="btnContact">Contact us</button></div>
+	</footer>
+   </body>
+</html>
---
+++ PHP-IIM50300/header.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+$content = <<<CONTENT
+<!DOCTYPE html>
+
+<html>
+   <head>
+   		<title>Music database | Front page</title>
+   		<link rel="stylesheet" type="text/css" href="style.css">	
+   </head>
+
+   <body>
+         <nav>
+            <div id="headerBar"><span id="title">Music database</span>
+               <button class="buttons" id="btnLogin">LOGIN</button>
+               <button class="buttons" id="btnSignUp">SIGN UP</button>
+            </div>
+            <div id="searchBar">
+               <form>
+                  <input id="inputSearch" type="text" name="search">
+                  <input class="buttons" id="btnSearchFromDatabase" type="submit" name="btnSearchDatabase" value="Search from database">
+               </form> 
+            </div>            
+         </nav>
+         <div id="container">
+            <div id="sideBar">
+            	<table>
+            		<tr><td>Home</td></tr>
+            		<tr><td>Artists</td></tr>
+            		<tr><td>Albums</td></tr>
+            		<tr><td>Tracks</td></tr>
+            		<tr><td>Genres</td></tr>
+            		<tr><td>Record companies</td></tr>
+            		<tr><td>About</td></tr>
+            	</table>
+            </div>	  
+
+CONTENT;
+
+
+echo $content;
+
+?>
